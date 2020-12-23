d1e4026f9468211c9af202f5829543c49b105f3e
System Administrator <root@macbooks-MacBook-Pro.local>
2020-12-23 15:04:42 +0200

    check2

--- apps/Notes/NoteApp.jsx
+++ apps/Notes/NoteApp.jsx
@@ -1,10 +1,12 @@
 export class NoteApp extends React.Component {
 
+  
+
   render() {
     return (
-      <div>
+      <section>
         <h1>Keep app</h1>
-      </div>
+      </section>
     )
   }
 }
