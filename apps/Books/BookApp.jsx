14f4dc9a737033d5c6844d857318c43807d6c615
Almog balila <almogbalila21@gmail.com>
2020-12-26 17:42:39 +0200

    adding books filter

--- apps/Books/BookApp.jsx
+++ apps/Books/BookApp.jsx
@@ -24,14 +24,52 @@ export class BookApp extends React.Component {
          });
        
     }
+    getBooksToShow = () => {
+        const { books } = this.state;
+        return books;
+    }
+    onRemoveBook = (bookId) => {
+        bookService.remove(bookId).then(() => {
+            this.loadBooks()
+        })
+    }
+
+    // getBookForDisplay = () => {
+    //     const { filterBy } = this.state;
+    //     //Another way of doing filter
+    //     // const filterRegex = new RegExp(filterBy.name,'i');
+    //     // return this.state.pets.filter(pet=> filterRegex.test(pet.name));
+
+    //     return this.state.books.filter(book => {
+
+    //         return book.title.toLowerCase().includes(filterBy.title.toLowerCase());
+    //     }).filter(book => {
+    //         return book.listPrice.amount >= filterBy.fromPrice && book.listPrice.amount <= filterBy.toPrice
+    //     })
+    
+    // };
+    getBookForDisplay = () => {
+        const { filterBy } = this.state;
+
+        return this.state.books.filter(book => {
+            return book.title.toLowerCase().includes(filterBy.title.toLowerCase()) &&
+                book.listPrice.amount >= filterBy.fromPrice &&
+                book.listPrice.amount <= filterBy.toPrice
+        })
+    };
+
+    onSetFilter = (filterBy) => {
+        this.setState({ filterBy })
+    }
+
 
 
 render() {
      
         return (
             <section className="book-app">
-        <h1>miss book</h1>
-        <BookList books={this.state.books}  onRemove={this.onRemovePet}/>
+        <BookFilter filterBy={this.state.filterBy} onSetFilter={this.onSetFilter} />
+        <BookList books={this.getBookForDisplay()}  onRemove={this.onRemoveBook}/>
             </section>
         );
     }
--- apps/Books/cmps/BookDetails.jsx
+++ apps/Books/cmps/BookDetails.jsx
@@ -1,8 +1,8 @@
 
 import { bookService } from "../services/book-service.js";
-// import { ReviewAdd } from "./review-add.jsx";
+ import { ReviewAdd } from "./BookReviewAdd.jsx"
 import { LongTxt } from "./BookLongTxt.jsx";
-// import { ReviewDetails } from "./review-details.jsx"
+ import { ReviewDetails } from "./BookReview.jsx"
 const { Link } = ReactRouterDOM;
 
 
@@ -59,11 +59,11 @@ export class BookDetails extends React.Component {
     onCloseModal = () => {
         this.props.history.push('/book');
     }
-    // addReview = (reviewToAdd) => {
-    //     console.log(reviewToAdd);
-    //     bookService.save(this.state.book, reviewToAdd).then(book => this.setState({ book }))
-    //     // this.loadBook()
-    // }
+    addReview = (reviewToAdd) => {
+        console.log(reviewToAdd);
+        bookService.save(this.state.book, reviewToAdd).then(book => this.setState({ book }))
+        // this.loadBook()
+    }
 
 
     render() {
@@ -73,12 +73,16 @@ export class BookDetails extends React.Component {
         let btnText = isLongTxtShown ? 'show less' : 'show more';
         const { reviews } = this.state.book
         return (
-            <div className="modal">
-                {book.listPrice.isOnSale && <img className="sale-img" src="../assets/img/sale.png" />}
+            <div>
+                <div className="prev-book">  <Link to={`/book/${bookService.getPrevBook(book.id)}`}>
+                <i className="fa fa-chevron-circle-left" style={{ color: 'rgb(68, 68, 68)'}}></i> Prev Book
+                 </Link></div>
+            <div className="book-details">
+                {book.listPrice.isOnSale && <img className="sale-img" src="../../../assets/img/sale.png" />}
                 <h1>title: {book.title}</h1>
                 <h2>subtitle: {book.subtitle}</h2>
                 <h3>{this.checkPageCount()}</h3>
-                <img src={book.thumbnail} alt="" />
+                <img  className="book-img" src={book.thumbnail} alt="" />
                 <p>authors: {book.authors}</p>
                 <p>publishedDate: {book.publishedDate} </p>
                 <p>pageCount: {book.pageCount} type:{this.checkPublishedDate()}</p>
@@ -89,21 +93,17 @@ export class BookDetails extends React.Component {
                 <p>categories: {book.categories}</p>
                 <p className={this.checkPrice()}>{book.listPrice.amount}<i className={`fa fa-${bookService.getCurrency(book.listPrice.currencyCode)}-sign`}></i></p>
                 <button onClick={this.getReview}> add review</button>
-                {/* <ReviewAdd addReview={this.addReview} /> */}
-                {/* <ReviewDetails book={book}/> */}
-                {/* {reviews && <ReviewDetails book={book} />} */}
-                <Link to={`/book/${bookService.getPrevBook(book.id)}`}>
-                    Prev Book
-                 </Link>|
-                <Link to={`/book/${bookService.getNextBook(book.id)}`}>
-                    Next Book
+                {/* <ReviewAdd addReview={this.addReview} />
+               
+                {reviews && <ReviewDetails book={book} />} */}
+                <button className="btn-close-details" onClick={this.onCloseModal}><i className="fa fa-window-close"></i></button>
+
+            </div>
+                <div className="next-book">
+                    <Link to={`/book/${bookService.getNextBook(book.id)}`}>
+                        Next Book <i className="fa fa-chevron-circle-right" style={{ color: 'rgb(68, 68, 68)'}}></i>
                  </Link>
-
-
-
-
-                <button className="btn-close-modal" onClick={this.onCloseModal}><i className="fa fa-window-close"></i></button>
-
+                </div>
             </div>
         )
 
