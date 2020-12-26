f3115167c89733ee48179f2d0b87045d33e9b9c5
Almog balila <almogbalila21@gmail.com>
2020-12-26 20:06:37 +0200

    adding header

--- apps/Books/cmps/BookReviewAdd.jsx
+++ apps/Books/cmps/BookReviewAdd.jsx
@@ -4,10 +4,10 @@ import { eventBusService } from "../../../../services/eventBusService.js";
 export class BookReviewAdd extends React.Component {
 
     state = {
-        review: { fullName: 'Books Reader', rate: 0, datepicker: '',commend:'' }
+        review: { fullName: 'Books Reader', rate: 0, datepicker: '', commend: '' }
     };
 
-  
+
 
     onInputChange = (ev) => {//on input change
 
@@ -19,49 +19,53 @@ export class BookReviewAdd extends React.Component {
 
         this.setState({
             review: bookCopy
-        },()=>console.log(this.state));
+        }, () => console.log(this.state));
     };
-    onChangeRate = (diff) => {
-        
+    onChangeRate = (diff, ev) => {
+        ev.preventDefault();
+
+
         const { review } = this.state
 
-        if (review.rate+diff<0||review.rate+diff>5) {
-         return
+        if (review.rate + diff < 0 || review.rate + diff > 5) {
+            return
         }
         else {
             var copy = { ...review }
             copy.rate += diff
             this.setState({
                 review: copy
-            },()=>console.log(this.state))
+            }, () => console.log(this.state))
         }
     }
-    onSaveReview =(ev)=>{
+    onSaveReview = (ev) => {
         ev.preventDefault();
+
         this.props.addReview(this.state.review)
-        eventBusService.emit('showMsg', { type: 'success', txt: `Book was successfully add`})
+        eventBusService.emit('showMsg', { type: 'success', txt: `Book was successfully add` })
+        this.setState({ review: { ...this.state.review, fullName: '',datepicker:'',commend:'' } })
     }
 
 
     render() {
         return (
             <div className="book-review-add">
-               
-            <form onSubmit={this.onSaveReview}>
-                <div className="book-review-add-input">
-                <input value={this.state.review.fullName}  placeholder="Name" type="text" name="fullName" onChange={this.onInputChange} />
-                <input type="date"  name="datepicker" onChange={this.onInputChange} />
-                </div>
-                <button  className="btn-rate" onClick={() => this.onChangeRate(-1)}>-</button>
-                     rate: {this.state.review.rate}
-                <button className="btn-rate"  onClick={() => this.onChangeRate(1)}>+</button>
-                <div className="book-review-info">
 
-                <textarea id="w3review" name="commend" rows="4" cols="20" onChange={this.onInputChange}></textarea>
-                <button  className="btn-submit" type="submit">Add Review</button>
-                </div>
-            </form>
-            
+                <form onSubmit={this.onSaveReview}>
+                    <div className="book-review-add-input">
+                        <input value={this.state.review.fullName} placeholder="Name" type="text" name="fullName" onChange={this.onInputChange} />
+                        <input   value={this.state.review.datepicker} type="date" name="datepicker" onChange={this.onInputChange} />
+                    </div>
+                    <button className="btn-rate" onClick={(ev) => this.onChangeRate(-1, ev)}>-</button>
+                     rate: {this.state.review.rate}
+                    <button className="btn-rate" onClick={(ev) => this.onChangeRate(1, ev)}>+</button>
+                    <div className="book-review-info">
+
+                        <textarea   value={this.state.review.commend} id="w3review" name="commend" rows="4" cols="20" onChange={this.onInputChange}></textarea>
+                        <button className="btn-submit" type="submit">Add Review</button>
+                    </div>
+                </form>
+
             </div>
 
         );
